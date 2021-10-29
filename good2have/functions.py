c51dffbbc9a63624f2d390279d011980c2b254bb
Nikolaoos <66828998+Nikolaoos@users.noreply.github.com>
2021-10-29 10:26:58 +0200

    first commit

---
+++ good2have/functions.py
@@ -0,0 +1,12 @@
+def getInputBetween(startval: int,endval: int)->int:
+    while True:
+        try:
+            val=int(input("Mata in:"))
+            if val >= startval and val <= endval:
+                return val
+            print(f"Ogiltigt val, mellan {startval} och {endval}, tack")
+        except:
+            print("Ange ett tal tack!")
+
+x = getInputBetween(100, 200)
+print(x)
